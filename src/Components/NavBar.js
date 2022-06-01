ca8997288894250994fa9c2364aa375c75fdab7e
johnyoun <johnyoun28@gmail.com>
2022-05-31 20:12:25 -0700

    left side of nav bar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,29 +1,57 @@
+import { Search } from '@mui/icons-material';
 import React from 'react'
 import styled from 'styled-components';
 
 const Container = styled.div`
-    height: 50px;
+    height: 40px;
 `
 const Wrapper = styled.div`
     padding: 10px 20px;
     display: flex;
     justify-content: space-between ;
+    align-items:center;
 `
 const Left = styled.div`
-    color: red;
+    flex: 1;
+    display: flex;
+    align-items: center;
 `
+const Language = styled.span`
+    font-size: 14px;
+    cursor: pointer ;
+`
+const SearchContainer = styled.div`
+    border: 1px solid lightgray;
+    display: flex;
+    align-items: center;
+    margin-left: 25px;
+    padding: 5px;
+`
+
+const Input = styled.input`
+    border: none;
+`
+
 const Center = styled.div`
     color: red;
+    flex: 1;
 `
 const Right = styled.div`
     color: red;
+    flex: 1;
 `
 
 const NavBar = () => {
   return (
     <Container>
         <Wrapper>
-            <Left>Left</Left>
+            <Left>
+                <Language>EN</Language>
+                <SearchContainer>
+                   <Input/> 
+                   <Search/>
+                </SearchContainer>
+            </Left>
             <Center>Center</Center>
             <Right>Right</Right>
         </Wrapper>
