42285c162a39e8b031f4c52083bb303c5a98ce31
johnyoun <johnyoun28@gmail.com>
2022-06-02 20:47:08 -0700

    arrows

--- src/Components/Slider.js
+++ src/Components/Slider.js
@@ -6,6 +6,8 @@ const Container = styled.div`
     width: 100%;
     height: 100vh;
     display: flex;
+    background-color: coral;
+    position: relative;
 `
 const Arrow = styled.div`
     width: 50px;
@@ -15,6 +17,10 @@ const Arrow = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
+    position: absolute;
+    top:0;
+    bottom:0;
+    margin: auto;
 `
 
 
