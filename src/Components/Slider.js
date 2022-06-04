28394e1bf8351891bf4b2c0248632d1dd9bbe09b
johnyoun <johnyoun28@gmail.com>
2022-06-03 20:52:08 -0700

    slider arrows made

--- src/Components/Slider.js
+++ src/Components/Slider.js
@@ -18,11 +18,13 @@ const Arrow = styled.div`
     align-items: center;
     justify-content: center;
     position: absolute;
-    top:0;
-    bottom:0;
+    top: 0;
+    bottom: 0;
     left: ${props => props.direction === 'left' && '10px'};
     right: ${props => props.direction === 'right' && '10px'};
     margin: auto;
+    cursor: pointer;
+    opacity: 0.5;
 `
 
 
