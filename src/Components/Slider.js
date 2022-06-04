bad8c491468a653f5686b13b9118e7baab798323
johnyoun <johnyoun28@gmail.com>
2022-06-03 21:26:33 -0700

    image and info

--- src/Components/Slider.js
+++ src/Components/Slider.js
@@ -6,13 +6,12 @@ const Container = styled.div`
     width: 100%;
     height: 100vh;
     display: flex;
-    background-color: coral;
     position: relative;
 `
 const Arrow = styled.div`
     width: 50px;
     height: 50px;
-    background-color: white;
+    background-color: #fff7f7;
     border-radius: 50%;
     display: flex;
     align-items: center;
@@ -27,6 +26,45 @@ const Arrow = styled.div`
     opacity: 0.5;
 `
 
+const Wrapper = styled.div`
+    height: 100%;
+`
+
+const Slide = styled.div`
+    display: flex;
+    align-items: center;
+    width: 100vw;
+    height: 100vh;
+`
+const ImgContainer = styled.div`
+    flex: 1;
+    height: 100%;
+`
+const Image = styled.img`
+    height: 80%;
+`
+const InfoContainer = styled.div`
+    flex: 1;
+    padding: 50px;
+`
+const Title = styled.h1`
+    font-size: 70px;
+`
+const Desc = styled.p`
+    margin: 5% 0;
+    font-size: 20px;
+    font-weight: 500;
+    letter-spacing: 3px;
+
+`
+const Button = styled.button`
+    padding: 2%;
+    background-color: transparent;
+    cursor: pointer;
+    font-size: 20px;
+`
+
+
 
 const Slider = () => {
   return (
@@ -35,6 +73,22 @@ const Slider = () => {
        <Arrow direction='left'>
             <ArrowLeftOutlined />
         </Arrow>
+
+        <Wrapper>
+            <Slide>
+                <ImgContainer>
+                <Image src="https://www.nicepng.com/png/full/8-88585_women-fashion-png-example-of-magazine-cover.png"/>
+                </ImgContainer>
+            <InfoContainer>
+                <Title>SUMMER SALE!</Title>
+                <Desc>DON'T COMPRMOISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.</Desc>
+                <Button>SHOW NOW</Button>
+            </InfoContainer>
+
+
+            </Slide>
+
+        </Wrapper>
 
         <Arrow direction='right'>
             <ArrowRightOutlined />
