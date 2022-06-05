6c1edfa7dcbc9947f1526ee99a63adc4a71b8240
johnyoun <johnyoun28@gmail.com>
2022-06-04 21:47:13 -0700

    readme

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import Announcement from '../Components/Announcement'
 import NavBar from '../Components/NavBar'
+import Slider from '../Components/Slider'
 
 const Home = () => {
   return (
-    <div><NavBar />
+    <div>
+      <Announcement />
+      <NavBar />
+      <Slider />
+
     </div>
   )
 }
