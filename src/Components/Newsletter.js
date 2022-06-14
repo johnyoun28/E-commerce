60f7adc8db10add7d9cc6b9ed31702c018d21c44
johnyoun <johnyoun28@gmail.com>
2022-06-13 21:22:12 -0700

    newsletter displayed

--- src/Components/Newsletter.js
+++ src/Components/Newsletter.js
@@ -1,8 +1,34 @@
+import { Send } from '@mui/icons-material'
 import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+    background-color: #fcf5f5;
+    height: 60vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+`
+const Title = styled.h1``
+const Desc = styled.div``
+const InputContainer = styled.div``
+const Input = styled.input``
+const Button = styled.button``
 
 const Newsletter = () => {
   return (
-    <div>Newsletter</div>
+      <Container>
+          <Title>Newsletter</Title>
+          <Desc>Get timely updates from your favorite products.</Desc>
+          <InputContainer>
+            <Input placeholder='Your email'/>
+            <Button>
+                <Send/>
+            </Button>
+          </InputContainer>
+      </Container>
+
   )
 }
 
