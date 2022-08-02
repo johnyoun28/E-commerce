6b91c00df722ae97584293202f5c4f2567ebd4b8
johnyoun <johnyoun28@gmail.com>
2022-08-01 20:50:54 -0700

    product list

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import './App.css';
 import Home from './Pages/Home';
+import ProductList from './Pages/ProductList';
 
 function App() {
   return (
-    <Home />
+    <ProductList/>
   );
 }
 
