097ce8820733ce0eaaab95fc7e3a1aef58cabca3
johnyoun <johnyoun28@gmail.com>
2022-08-11 20:50:59 -0700

    filter and sizing

--- src/Pages/Product.js
+++ src/Pages/Product.js
@@ -6,14 +6,65 @@ import Footer from '../Components/Footer'
 import NavBar from '../Components/NavBar'
 import Newsletter from '../Components/Newsletter'
 
-const Container = styled.div``
-const Wrapper = styled.div``
-const ImgContainer = styled.div``
-const Image = styled.img``
-const InfoContainer = styled.div``
-const Title = styled.h1``
-const Desc = styled.p``
-const Price = styled.span``
+const Container = styled.div`
+
+`
+const Wrapper = styled.div`
+    padding: 50px;
+    display: flex;
+`
+const ImgContainer = styled.div`
+    flex: 1;
+`
+const Image = styled.img`
+    width: 100%;
+    height: 90vh;
+    object-fit: cover;
+`
+const InfoContainer = styled.div`
+    flex: 1;
+    padding: 0px 50px;
+`
+const Title = styled.h1`
+    font-weight: 200;
+`
+
+const Desc = styled.p`
+    margin: 20px 0;
+`
+const Price = styled.span`
+    font-weight: 200;
+    font-size: 40px;
+`
+
+const FilterContainer = styled.div`
+    width: 45%;
+    margin: 30px 0;
+    display: flex;
+    justify-content: space-between;
+`
+const Filter = styled.div`
+    display: flex;
+    align-items: center;
+`
+const FilterTitle = styled.span`
+    font-size: 20px;
+    font-weight: 200;
+`
+const FilterColor = styled.div`
+    width: 20px;
+    height: 20px;
+    border-radius: 50%;
+    background-color: ${props => props.color};
+    margin: 0 5px;
+    cursor: pointer;
+`
+const FilterSize = styled.select`
+    margin-left: 10px;
+    padding: 5px;
+`
+const FilterSizeOption = styled.option``
+
 
 
 
@@ -28,8 +79,29 @@ const Product = () => {
             </ImgContainer>
             <InfoContainer>
                 <Title>Denim Jumpsuit</Title>
-                <Desc>Lorem Ipsum</Desc>
-                <Price>$20</Price>
+                <Desc>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</Desc>
+                <Price>$ 20</Price>
+                <FilterContainer>
+                    <Filter>
+                        <FilterTitle>Color</FilterTitle>
+                        <FilterColor color='black' />
+                        <FilterColor color='darkblue' />
+                        <FilterColor color='gray' />
+                    </Filter>
+                    <Filter>
+
+                        <FilterTitle>Size</FilterTitle>
+                        <FilterSize>
+                            <FilterSizeOption>S</FilterSizeOption>
+                            <FilterSizeOption>M</FilterSizeOption>
+                            <FilterSizeOption>L</FilterSizeOption>
+                            <FilterSizeOption>S</FilterSizeOption>
+                            <FilterSizeOption>S</FilterSizeOption>
+                        </FilterSize>
+                        
+                    </Filter>
+
+                </FilterContainer>
             </InfoContainer>
         </Wrapper>
         <Newsletter/>
