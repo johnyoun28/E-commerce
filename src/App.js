1bf10740247c4c4bca41079afef1de54093c18ab
johnyoun <johnyoun28@gmail.com>
2022-08-17 21:03:29 -0700

    register page start

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import './App.css';
 import Home from './Pages/Home';
 import Product from './Pages/Product';
 import ProductList from './Pages/ProductList';
+import Register from './Pages/Register';
 
 function App() {
   return (
-    <Product/>
+    <Register/>
   );
 }
 
