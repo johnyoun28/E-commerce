c66ef9fa89e7c68a82285482bbb37155c2633383
johnyoun <johnyoun28@gmail.com>
2022-08-30 19:32:24 -0700

    start on register

--- src/Pages/Register.js
+++ src/Pages/Register.js
@@ -1,12 +1,39 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const Container = styled.div``
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: linear-gradient(
+    rgba(255,255,255,0.5), rgba(255,255,255,0.5)), url('https://images.pexels.com/photos/6984661/pexels-photo-6984661.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940') center;
+`
+const Wrapper = styled.div``
+const Form = styled.form``
+const Title = styled.h1``
+const Input = styled.input``
+const Agreement = styled.span``
+const Button = styled.button``
+
 
 const Register = () => {
   return (
     <Container>
-      Register
+      <Wrapper>
+        <Title>CREATE AN ACCOUNT</Title>
+        <Form>
+          <Input placeholder='name'/>
+          <Input placeholder='name'/>
+          <Input placeholder='name'/>
+          <Input placeholder='name'/>
+          <Input placeholder='name'/>
+          <Input placeholder='name'/>
+          <Agreement>
+            By creating an account, I consent to the processing of my personal data in accordance with the <b>PRIVATE POLICY</b>
+          </Agreement>
+          <Button>CREATE</Button>
+
+        </Form>
+      </Wrapper>
     </Container>
   )
 }
