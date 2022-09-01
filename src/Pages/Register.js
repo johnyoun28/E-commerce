bd1e139608ac2495dc0e2669d4a32a845fe33586
johnyoun <johnyoun28@gmail.com>
2022-08-31 23:33:18 -0700

    register page

--- src/Pages/Register.js
+++ src/Pages/Register.js
@@ -6,13 +6,42 @@ const Container = styled.div`
   height: 100vh;
   background: linear-gradient(
     rgba(255,255,255,0.5), rgba(255,255,255,0.5)), url('https://images.pexels.com/photos/6984661/pexels-photo-6984661.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940') center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 `
-const Wrapper = styled.div``
-const Form = styled.form``
-const Title = styled.h1``
-const Input = styled.input``
-const Agreement = styled.span``
-const Button = styled.button``
+const Wrapper = styled.div`
+  padding: 20px;
+  width: 40%;
+  background-color: white;
+`
+
+const Title = styled.h1`
+  font-size: 24px;
+  font-weight: 300;
+`
+const Form = styled.form`
+  display: flex;
+  flex-wrap: wrap;
+`
+const Input = styled.input`
+  flex: 1;
+  min-width: 40%;
+  margin: 20px 10px 0 0;
+  padding: 10px;
+`
+const Agreement = styled.span`
+  font-size: 12px;
+  margin: 20px 0;
+`
+const Button = styled.button`
+  width: 40%;
+  border: none;
+  padding: 15px 20px;
+  background-color: teal;
+  color: white;
+  cursor: pointer;
+`
 
 
 const Register = () => {
@@ -22,11 +51,11 @@ const Register = () => {
         <Title>CREATE AN ACCOUNT</Title>
         <Form>
           <Input placeholder='name'/>
-          <Input placeholder='name'/>
-          <Input placeholder='name'/>
-          <Input placeholder='name'/>
-          <Input placeholder='name'/>
-          <Input placeholder='name'/>
+          <Input placeholder='last name'/>
+          <Input placeholder='username'/>
+          <Input placeholder='email'/>
+          <Input placeholder='password'/>
+          <Input placeholder='confirm password'/>
           <Agreement>
             By creating an account, I consent to the processing of my personal data in accordance with the <b>PRIVATE POLICY</b>
           </Agreement>
