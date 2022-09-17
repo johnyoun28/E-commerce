ce719de98e642b9452115857cfbde6716f67202b
johnyoun <johnyoun28@gmail.com>
2022-09-16 19:02:50 -0700

    product iphone mobile

--- src/Pages/Product.js
+++ src/Pages/Product.js
@@ -6,6 +6,7 @@ import Announcement from '../Components/Announcement'
 import Footer from '../Components/Footer'
 import NavBar from '../Components/NavBar'
 import Newsletter from '../Components/Newsletter'
+import { mobile } from '../responsive'
 
 const Container = styled.div`
 
@@ -13,6 +14,7 @@ const Container = styled.div`
 const Wrapper = styled.div`
     padding: 50px;
     display: flex;
+    ${mobile({padding: '10px 0', flexDirection: 'column'})}
 `
 const ImgContainer = styled.div`
     flex: 1;
@@ -21,10 +23,12 @@ const Image = styled.img`
     width: 100%;
     height: 90vh;
     object-fit: cover;
+    ${mobile({height: '40%'})}
 `
 const InfoContainer = styled.div`
     flex: 1;
     padding: 0px 50px;
+    ${mobile({padding: '10px'})}
 `
 const Title = styled.h1`
     font-weight: 200;
@@ -43,6 +47,7 @@ const FilterContainer = styled.div`
     margin: 30px 0;
     display: flex;
     justify-content: space-between;
+    ${mobile({width: '100%'})}
 `
 const Filter = styled.div`
     display: flex;
@@ -71,6 +76,7 @@ const AddContainer = styled.div`
     align-items: center;
     width: 50%;
     justify-content: space-between;
+    ${mobile({width: '100%'})}
 `
 const AmountContainer = styled.div`
     display: flex;
